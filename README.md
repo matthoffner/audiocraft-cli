194dd36576781599e04258674cf0d0c3d9b64c70
matt HOFFNER <matthoffner@matts-Mac-Studio.local>
2023-08-20 14:28:42 -0700

    init

---
+++ README.md
@@ -0,0 +1,7 @@
+# audiocraft-pod
+
+Space for developing some ideas on AudioCraft.
+
+# v0
+
+Copied example from Github issue, added some command line args and timer
--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-import torchaudio
 import datetime
 import time
 import argparse
@@ -7,28 +6,33 @@ from audiocraft.models import MusicGen
 from audiocraft.data.audio import audio_write
 
 parser = argparse.ArgumentParser(description="Generate music with a specified duration.")
-parser.add_argument("--duration", type=int, default=2, help="Duration of the generated music in seconds.")
+parser.add_argument("--duration", type=int, default=5, help="Duration of the generated music in seconds.")
+parser.add_argument("--prompt", type=str, required=True, help="Prompt for music to generate.")
+parser.add_argument("--use_sampling", type=bool, default=True, help="Use sampling during generation.")
+parser.add_argument("--top_k", type=int, default=250, help="Value for top_k during generation.")
+parser.add_argument("--top_p", type=float, default=1, help="Value for top_p during generation.")
+parser.add_argument("--temperature", type=float, default=0.8, help="Value for temperature during generation.")
+parser.add_argument("--cfg_coef", type=float, default=9.0, help="Value for cfg_coef during generation.")
+parser.add_argument("--extend_stride", type=int, default=20, help="Stride value during extension.")
 args = parser.parse_args()
 duration = args.duration
+prompt = args.prompt
 
 start_time = time.time()
 current_date_time = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
 
 model = MusicGen.get_pretrained('facebook/musicgen-small')
 model.set_generation_params(
-    use_sampling=True,
-    top_k=250, #int
-    top_p=1, #float
-    temperature=0.8, #float
-    cfg_coef=9.0,  #float
-    #extend_stride=20, 
-    duration=duration # generate x seconds
-)  
-descriptions = [
-    ''
-]
+    use_sampling=args.use_sampling,
+    top_k=args.top_k,
+    top_p=args.top_p,
+    temperature=args.temperature,
+    cfg_coef=args.cfg_coef,
+    extend_stride=args.extend_stride,
+    duration=duration  # generate x seconds
+)
 
-wav = model.generate(descriptions)  # generates as many samples as in comma separated descriptions
+wav = model.generate(prompt)  # generates as many samples as in comma separated descriptions
 
 for idx, one_wav in enumerate(wav):
     file_name = f'{idx}_{current_date_time}'
@@ -36,15 +40,15 @@ for idx, one_wav in enumerate(wav):
     audio_write(
         file_name,
         one_wav.cpu(),
-        format = "wav",
-        sample_rate = model.sample_rate,
-        normalize = True,
-        strategy = "loudness", #clip, peak, rms, loudness
-        loudness_headroom_db = 16,
-        loudness_compressor = True,
-        log_clipping = True #only when strategy = loudness
+        format="wav",
+        sample_rate=model.sample_rate,
+        normalize=True,
+        strategy="loudness",  # clip, peak, rms, loudness
+        loudness_headroom_db=16,
+        loudness_compressor=True,
+        log_clipping=True  # only when strategy = loudness
     )
 
 end_time = time.time()
 elapsed_time = end_time - start_time
-print(f"Generated {descriptions}, duration: {duration}s, elapsed time: {elapsed_time:.2f}s")
+print(f"Generated {prompt}, duration: {duration}s, elapsed time: {elapsed_time:.2f}s")
